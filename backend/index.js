ef66d6a573dbf146fa4e94cb50a2d0cf8fc888c5
Prakharshrivastava <prakharshrivastava0702@gmail.com>
2024-10-03 21:52:00 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,6 @@ mongoose.connect(process.env.MONGO_URL, {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is Running on port ${PORT}`);
 });
