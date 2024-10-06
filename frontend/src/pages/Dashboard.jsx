89817256db236e9096c88b8eadb8823fc117caf2
Prakharshrivastava <prakharshrivastava0702@gmail.com>
2024-10-06 23:46:51 +0530

    debug

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
                 const res = await axios.get('http://13.201.94.103:3000/auth/verify');
                 // const res = await axios.get(`${API_URL}/auth/verify`);
                 console.log(res);
-                if (res.data.status) {
+                if (res.status) {
                     setIsAuthenticated(true);
                     setIsAdmin(res.data.isAdmin); // Assuming your API returns this info
                     fetchUserStats();
